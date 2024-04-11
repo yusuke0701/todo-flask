0300720957b5bc475e532f19906dcc23c5591c25
Yusuke Nakashima <yuusukerego@yahoo.co.jp>
2024-04-11 03:41:30 +0000

    TODOアプリ完成

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object("config")
 
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+
+    new_todo = Todo(title=title)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    return redirect(url_for("home"))
+
+
+@app.route("/delete/<int:todo_id>", methods=["POST"])
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run()
